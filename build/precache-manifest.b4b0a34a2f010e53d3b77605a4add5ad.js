3da6175c8e50b6ab0edab794592ce4f414f5060f
Dwayne Brown <dwayne@pebr.co.uk>
2021-04-01 12:36:43 +0100

    vercel build https://build-ruddy-beta.vercel.app/

---
+++ build/precache-manifest.b4b0a34a2f010e53d3b77605a4add5ad.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3272a255ed181ec86345faa6612e3d9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bc857d2746eadb5596f",
+    "url": "/static/css/2.11b452ed.chunk.css"
+  },
+  {
+    "revision": "f09d4bf312612181d377",
+    "url": "/static/css/main.1aa3f63c.chunk.css"
+  },
+  {
+    "revision": "4bc857d2746eadb5596f",
+    "url": "/static/js/2.8858d2bd.chunk.js"
+  },
+  {
+    "revision": "0601554d6437e8ab1ea2021021d2eb3b",
+    "url": "/static/js/2.8858d2bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f09d4bf312612181d377",
+    "url": "/static/js/main.b403b3c8.chunk.js"
+  },
+  {
+    "revision": "0231f95ac667b73c9187",
+    "url": "/static/js/runtime-main.91e21c86.js"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
